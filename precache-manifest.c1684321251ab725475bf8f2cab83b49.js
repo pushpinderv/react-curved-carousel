0eca6c365c7a35e456ab0ee68b0b489aa2eff7df
Pushpinder Virdi <pushpindervirdi94@gmail.com>
2020-08-06 05:41:04 -0400

    Updates

---
+++ precache-manifest.c1684321251ab725475bf8f2cab83b49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d2ab23388d01d64c8958be335f5fab9",
+    "url": "/react-curved-carousel/index.html"
+  },
+  {
+    "revision": "8003cc6c8b19867e2df7",
+    "url": "/react-curved-carousel/static/css/main.3a7e2dc2.chunk.css"
+  },
+  {
+    "revision": "bd3e2db626aeda8c0780",
+    "url": "/react-curved-carousel/static/js/2.086a33a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-curved-carousel/static/js/2.086a33a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8003cc6c8b19867e2df7",
+    "url": "/react-curved-carousel/static/js/main.6e648af8.chunk.js"
+  },
+  {
+    "revision": "710aba72462ca11dc85f",
+    "url": "/react-curved-carousel/static/js/runtime-main.00371f60.js"
+  }
+]);
